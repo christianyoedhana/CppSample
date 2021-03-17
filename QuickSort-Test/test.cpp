b4ac48534d9b743c7e2d13000be0038022c0d384
christianyoedhana <christian.yoedhana@gmail.com>
2021-03-17 14:24:13 -0700

    Add unit test for my QuickSort
    Add a test to verify the partition functions. Put partition definition in its header file for exposure.

--- QuickSort-Test/test.cpp
+++ QuickSort-Test/test.cpp
@@ -1,6 +1,45 @@
 #include "pch.h"
+#include "QuickSort/Partition.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+//checks if all values left of the pivot is less than or equal to the pivot. Values to the right of the pivot must be greater than the pivot.
+//T must support ::size_type using, < and > operator.
+//When the first value in the return tuple is false, the proceeding value is the index of the first offending value that breaks the qsort partition rule
+//When the first value is true, the proceeding value returns pivotIndex
+template<class T>
+std::tuple<bool, typename T::size_type> isPartitionValid(T& list, typename T::size_type pivotIndex) {
+	using namespace std;
+	const auto pivot = list.at(pivotIndex);
+	//given the input, the lowly for loop is the best construct to get the job done.
+	//check left
+	//check left
+	for (auto index = 0; index < pivotIndex; ++index) {
+		//Value == pivot is a meh.
+		if (auto const value = list.at(index) > pivot) {
+			return make_tuple(false, index);
+		}
+	}
+	//Check right
+	for (auto index = pivot; index < list.size(); ++index) {
+		if (auto const value = list.at(index) < pivot) {
+			return make_tuple(false, index);
+		}
+	}
+
+	return make_tuple(true, pivotIndex);
+}
+
+//Test QuickSort partition in ascending order and descending order.
+TEST(QuickSortTest, PartitionTest) {
+	std::vector<int> listToPartition{ 15, -1, 22, 4, 10, 121, 1, -2, 5 };
+	auto firstRun(listToPartition);
+	
+	auto const ascending = [](auto const& left, auto const& right)->bool { return left <= right; };
+	auto pivotPos = partition(firstRun, 0, firstRun.size() - 1, ascending);
+	bool success = false;
+	auto offendingMember = pivotPos;
+	std::tie(success, offendingMember) = isPartitionValid(firstRun, pivotPos);
+	EXPECT_TRUE(success) << "Member " << firstRun[offendingMember] << " at " << offendingMember << " is in the wrong position relative to " << firstRun[pivotPos] << " at position " << pivotPos;
+	pivotPos = partitionNoCast(listToPartition, 0, listToPartition.size() - 1, ascending); 
+	std::tie(success, offendingMember) = isPartitionValid(listToPartition, pivotPos);
+	EXPECT_TRUE(success) << "Member " << listToPartition[offendingMember] << " at " << offendingMember << " is in the wrong position relative to " << listToPartition[pivotPos] << " at position " << pivotPos;
 }
--- QuickSort/Partition.h
+++ QuickSort/Partition.h
@@ -1,4 +1,65 @@
 #pragma once
+#include <algorithm>
+#include <random>
+
+//Take pivot value from the end of the list
+//front will move forward while the value is less than pivot
+//back move backward while the value is greater or equal to pivot.
+//stop process after front > back
+//swap pivot position
+//This is inefficient because there are 4 conditional statements in the loop.
+//However, no typecasting from signed to unsigned for the container access index
+//This is a variant of Hoare's partition. But instead of while true, I check that front < back for a valid loop invariant
+template< class T, class Pred>
+typename T::size_type partitionNoCast(T& list, typename T::size_type front, typename T::size_type back, Pred const& compare)
+{
+	auto pivot = list.at(back);
+	const auto pivotPos = back;
+	while (front < back) {
+		//while (list.at(front) < pivot) front += 1;
+		while (compare(list.at(front), pivot)) front += 1;
+		while (!compare(list.at(back), pivot)) back -= 1;
+		if (back > front) {
+			std::swap(list.at(front), list.at(back));
+			front += 1;
+		}
+	}
+	//final swap
+	std::swap(list.at(pivotPos), list.at(front));
+	return front;
+}
+
+//Cormen's partition. Super efficient because there are only 2 conditional statement per loop
+//Still does not require type casting.
+//However, the largest partition size has been reduced to half because this implementation uses a signed int as an indexer
+//I can type cast the signed int to unsigned int. However, an overflow warning might be issue by the compiler.
+template< class T, class Pred>
+typename T::size_type partition(T& list, typename T::size_type front, typename T::size_type back, Pred const& compare)
+{
+	//nextToSwap points to the position prior to the potential swap index.
+	auto nextToSwap = front - 1;	//This is an underflow once, at the very beginning of the partition
+	auto pivot = list.at(back);
+	for (auto toSwap = front; toSwap < back; ++toSwap) {
+		if (compare(list.at(toSwap), pivot)) {
+			std::swap(list.at(toSwap), list.at(++nextToSwap));
+		}
+	}
+	std::swap(list.at(++nextToSwap), list.at(back));
+	return nextToSwap;
+}
+
+//I think Cormen's partition can be adapted to take an arbitrary pivot in the list
+//This one randomly select a pivot from front to back
+template <class T, class Pred>
+auto partitionArbitrary(T& list, typename T::size_type front, typename T::size_type back, Pred const& compare) {
+	using namespace std;
+	random_device seed;
+	mt19937 engine(seed());
+	uniform_int_distribution const gen{ front, back };
+	std::swap(list.at(gen(engine)), list.at(back));
+	return partition(list, front, back, compare);
+}
+
 void partitionTest();
 void arbitraryPartitionTest();
 void quicksortTest();
